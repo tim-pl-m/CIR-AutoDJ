ec6e26d3d8e441df4fe319c4a27ac98ed72c3b2f
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2017-12-17 14:49:19 +0100

    add stubbed learning-classe and method. q-learning will follow

---
+++ learning.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import random
+from random import randint
+from random import uniform
+
+class learning():
+
+genres = ["drum-and-bass",
+"edm",
+"electro",
+"electronic",
+"funk",
+"groove"]
+
+
+def getNextParamters(self):
+    random.shuffle(genres)
+
+    energy = uniform(0, 1)
+
+    vallance = uniform(0, 1)
+
+    bpm = randint(60, 180)
+
+    # print(genres[1])
+    # print(energy)
+    # print(bpm)
+    return genres, energy, vallance, bpm
+# Genre(String aus der Liste genres), Energy(0-1), Vallance(0-1), Beats per Minute(60-180)
+
+
+
