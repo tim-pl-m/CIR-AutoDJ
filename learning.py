aa47983fff119f3efd682ea8dbbf5e52d18cb6b7
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2018-01-16 08:58:10 +0100

    maxQ unclear

--- learning.py
+++ learning.py
@@ -4,7 +4,7 @@ import random
 from random import randint
 from random import uniform
 
-import numpy as np
+# import numpy as np
 from collections import defaultdict
 
 
@@ -24,35 +24,49 @@ class learning():
   # TODO fill in missing genres from file "genres"
 
   # q-learning parameter
-  learning_rate
-  discount_factor
+  learning_rate = 0.01
+  #TODO consider not myoptic(so make it longer sighted)
+  discount_factor = 0
 
   #matrix definitions
-  skalierung = 3
-  number_of_vertices = genres.length * skalierung
+  #subdivide genres
+  scaling = 3
+  number_of_vertices = genres.length * scaling
 
   #create matrix
   matrix = [[0] * number_of_vertices for _ in range(number_of_vertices)]
 
-  def add_edge(self, v1, v2):
+  #save actual vertice
+  # TODO consider starting from a random state
+  actual_state_x = 0
+  actual_state_y = 0
+
+  def adjustEdgeAndMatrix(self, x, y, reward):
       #in first iteation undirected
       # TODO consider directed
-            value = 1
+            old_value = matrix[x][y]
+            # plain q-learning forumla here
+            new_value = (1-learning_rate)*old_value + learning_rate*(reward+discount_factor*)
             #TODO add q-learning-value
-            matrix[v1][v2] = value
-            matrix[v2][v1] = value
+            matrix[x][y] = new_value
+            matrix[y][x] = new_value
             #TODO adjust slightly all values for the genre
             #TODO think about this
 
   def getNextParamters(self):
-    # getRandomParamters()
+    # pick next vertice
+    # TODO maybe catching errors is needed here
+    if matrix[actual_state_x][actual_state_y + 1]
 
+    #interpret matrix
     genre =
     bpm =
     #TODO consider reduction to genre+bpm
+    #pick unlearned values randomly
     energy = uniform(0, 1)
     vallance = uniform(0, 1)
 
+    # getRandomParamters()
     print(genres[0][0])
     print(energy)
     print(bpm)
@@ -62,6 +76,10 @@ class learning():
 
   # Current percieved mood between 0 and 1 (float)
   def learn(self, mood):
+    # calculate reward
+    # TODO
+    # adjust old value with reward
+    adjustEdgeAndMatrix(actual_state_x,actual_state_y, reward)
     pass
 
 def getRandomParamters(self):
